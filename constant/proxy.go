d54cabe84ede5ad934b81d73a006cef953b371be
lux5am <lux.5am@yandex.com>
2024-12-15 12:16:16 +0800

    add inner dns proxied connection statistic to restful api
    Signed-off-by: lux5am <lux.5am@yandex.com>

--- constant/proxy.go
+++ constant/proxy.go
@@ -1,6 +1,7 @@
 package constant
 
 const (
+	TypeInner        = "inner"
 	TypeTun          = "tun"
 	TypeRedirect     = "redirect"
 	TypeTProxy       = "tproxy"
@@ -41,6 +42,8 @@ const (
 
 func ProxyDisplayName(proxyType string) string {
 	switch proxyType {
+	case TypeInner:
+		return "Inner"
 	case TypeTun:
 		return "TUN"
 	case TypeRedirect:
