8ecdf4621feafd3977f2153a26a59533672947fd
lux5am <lux.5am@yandex.com>
2025-01-04 07:28:32 +0800

    add remotedst to clash restful api
    Signed-off-by: lux5am <lux.5am@yandex.com>

--- route/route.go
+++ route/route.go
@@ -134,6 +134,9 @@ func (r *Router) routeConnection(ctx context.Context, conn net.Conn, metadata ad
 	for _, buffer := range buffers {
 		conn = bufio.NewCachedConn(conn, buffer)
 	}
+	if len(r.trackers) > 0 {
+		metadata.InitExtended()
+	}
 	for _, tracker := range r.trackers {
 		conn = tracker.RoutedConnection(ctx, conn, metadata, selectedRule, selectedOutbound)
 	}
@@ -244,6 +247,9 @@ func (r *Router) routePacketConnection(ctx context.Context, conn N.PacketConn, m
 	for _, buffer := range packetBuffers {
 		conn = bufio.NewCachedPacketConn(conn, buffer.Buffer, buffer.Destination)
 		N.PutPacketBuffer(buffer)
+	}
+	if len(r.trackers) > 0 {
+		metadata.InitExtended()
 	}
 	for _, tracker := range r.trackers {
 		conn = tracker.RoutedPacketConnection(ctx, conn, metadata, selectedRule, selectedOutbound)
